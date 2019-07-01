8592631ceb724e3b537721b0e4880db7948fd83c
liuliang <454493675@qq.com>
2019-07-01 17:42:05 +0800

    完成 fragment 切换

--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.fragment;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.example.fragment.fragment.CartFragment;
+import com.example.fragment.fragment.HomeFragment;
+import com.example.fragment.fragment.MineFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     protected LinearLayout lHome , lCart, lMine;
+    protected Fragment f_home = new HomeFragment();
+    protected Fragment f_cart = new CartFragment();
+    protected Fragment f_mine = new MineFragment();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,16 +28,43 @@ public class MainActivity extends AppCompatActivity {
         lHome = findViewById(R.id.tabbar_home);
         lCart = findViewById(R.id.tabbar_cart);
         lMine = findViewById(R.id.tabbar_mine);
+
+        lHome.setSelected(true);
+
+        this.getSupportFragmentManager().beginTransaction()
+                .add(R.id.content_body,f_home)
+                .add(R.id.content_body,f_cart)
+                .add(R.id.content_body,f_mine)
+                .show(f_home)
+                .hide(f_mine)
+                .hide(f_cart)
+                .commit();
     }
 
     private void clearSelect() {
         lCart.setSelected(false);
         lHome.setSelected(false);
         lMine.setSelected(false);
+
+        this.getSupportFragmentManager().beginTransaction().hide(f_cart).hide(f_home).hide(f_mine).commit();
     }
 
     public void onClick(View view) {
         clearSelect();
         view.setSelected(true);
+        switch (view.getId()){
+            case R.id.tabbar_home:
+                this.getSupportFragmentManager().beginTransaction().show(f_home).commit();
+                break;
+            case R.id.tabbar_cart:
+                this.getSupportFragmentManager().beginTransaction().show(f_cart).commit();
+                break;
+            case R.id.tabbar_mine:
+                this.getSupportFragmentManager().beginTransaction().show(f_mine).commit();
+                break;
+        }
+
+
+
     }
 }
